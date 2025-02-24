38be5574efdda14211c429c51d49cc4c8a566a34
Diego Apolo <diegoapolo2011@gmail.com>
2025-02-24 09:10:29 -0500

    KAN-97: Send to owner normalize by card and account

--- src/common/utils/fullName.ts
+++ src/common/utils/fullName.ts
@@ -1,5 +1,23 @@
 import { Usuario } from '@/src/modules/users/users.entity'
 
 export const fullName = (user: Usuario) => {
-  return `${user.first_name} ${user.last_name}`
+  const firstName = user?.first_name
+  const lastName = user?.last_name || ''
+  if (!firstName) return ''
+
+  const first = firstName.split(' ')[0]
+  const lastParts = lastName.split(' ')
+  const last1 = lastParts[0]
+  const last2Initial = lastParts.length > 1 ? lastParts[1][0] + '.' : ''
+
+  let fullName = `${first} ${last1}`
+  if (last2Initial) {
+    fullName += ` ${last2Initial}`
+  }
+
+  if (first.length + last1.length > 20) {
+    return `${first} ${last1[0]}.`
+  }
+
+  return fullName
 }
